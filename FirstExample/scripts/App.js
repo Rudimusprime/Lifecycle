02ab6f8b2f0b3653b4a08e888f154a0f6633acb1
DanielB <d.banasiewicz@pdo.com.pl>
2021-10-26 20:32:21 +0200

    lifecycle methods

--- FirstExample/scripts/App.js
+++ FirstExample/scripts/App.js
@@ -1,11 +1,56 @@
 class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            number: 0,
+        }
+        console.log("Wywołana metoda constructor")
+    }
+
+    componentWillMount() {
+        console.log('Wywołana componentWillMount')
+    }
+
+    componentDidMount() {
+        console.log('Wywołana componentDidMount');
+        this.setState({
+            number: 1,
+        })
+    }
+
+    componentDidUpdate() {
+        console.log('Wywołana componentDidUpdate')
+    }
+
     render() {
+        console.log("Wywołana metoda render")
         return (
-            <p>
-                lifecycle - montowanie komponentu
-            </p>
+            <div>
+                <p>
+                    lifecycle - montowanie komponentu
+                </p>
+                <Child/>
+            </div>
         )
     }
 }
 
-ReactDOM.return(<App/>, document.getElementById('root'));
+class Child extends React.Component {
+    componentDidMount() {
+        console.log('Wywołana componentDidMount w komponencie Child');
+        // this.setState({
+        //     number: 1,
+        // })
+    }
+
+    render() {
+        console.log('Wywołana metoda render w komponencie Child');
+        return (
+            <h1>Dziecko</h1>
+        )
+    }
+
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
