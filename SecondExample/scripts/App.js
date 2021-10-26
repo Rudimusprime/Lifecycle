3b19a4764f8c2b4759e3eaf9a4c228b2ec021b04
DanielB <d.banasiewicz@pdo.com.pl>
2021-10-26 20:36:08 +0200

    second example initial

---
+++ SecondExample/scripts/App.js
@@ -0,0 +1,11 @@
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Second example</p>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
