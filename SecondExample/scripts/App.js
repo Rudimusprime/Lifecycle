f01ec8c2d348f30795d1034151af702d1ac750b7
DanielB <d.banasiewicz@pdo.com.pl>
2021-10-26 20:53:11 +0200

    extample on classes

--- SecondExample/scripts/App.js
+++ SecondExample/scripts/App.js
@@ -1,8 +1,68 @@
 class App extends React.Component {
+
+    state = {
+        status: true,
+    }
+
+    componentDidMount() {
+        console.log("Aplikacja zamontowana");
+    }
+
+    componentDidUpdate() {
+        console.log("Aplikacja zaktualizowana");
+    }
+
     render() {
+        console.log("Renderowanie aplikacji");
         return (
             <div>
-                <p>Second example</p>
+                <button onClick={() => this.setState({status: !this.state.status})}>Przełącz</button>
+                <Child1 status={this.state.status}/>
+                {this.state.status && <Child2/>}
+            </div>
+        )
+    }
+}
+
+class Child1 extends React.Component {
+
+    componentDidMount() {
+        console.log("Child1 zamontowany");
+    }
+
+    componentDidUpdate() {
+        console.log("Child1 zaktualizowana");
+    }
+
+    render() {
+        console.log("Renderowanie Child1");
+        return (
+            <div>
+                {String(this.props.status)}
+            </div>
+        )
+    }
+}
+
+class Child2 extends React.Component {
+
+    componentDidMount() {
+        console.log("Child2 zamontowany");
+    }
+
+    componentDidUpdate() {
+        console.log("Child2 zaktualizowana");
+    }
+
+    componentWillUnmount() {
+        console.log("Child2 odmontowany");
+    }
+
+    render() {
+        console.log("Renderiowanie Child2")
+        return (
+            <div>
+                Komponent Child2 zamontowany
             </div>
         )
     }
