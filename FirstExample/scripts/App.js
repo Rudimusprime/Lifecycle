59cc1b1662cff70fd601df7df0635c3b5cad65d9
DanielB <d.banasiewicz@pdo.com.pl>
2021-10-26 20:20:24 +0200

    Initial commit

---
+++ FirstExample/scripts/App.js
@@ -0,0 +1,11 @@
+class App extends React.Component {
+    render() {
+        return (
+            <p>
+                lifecycle - montowanie komponentu
+            </p>
+        )
+    }
+}
+
+ReactDOM.return(<App/>, document.getElementById('root'));
