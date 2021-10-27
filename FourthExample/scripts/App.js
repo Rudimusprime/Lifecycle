bb1cb009cbd73be26deebf22079e5447cbb6b8dd
DanielB <d.banasiewicz@pdo.com.pl>
2021-10-27 20:55:02 +0200

    initial fourth example and hotfix in third example

---
+++ FourthExample/scripts/App.js
@@ -0,0 +1,11 @@
+class Clock extends React.Component {
+    state = {}
+
+    render() {
+        render(
+            <div></div>
+        )
+    }
+}
+
+ReactDOM.render(<Clock/>, document.getElementById('root'));
