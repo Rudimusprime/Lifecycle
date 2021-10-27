aea6bcb5fdc40462e3a6c2b1d16aea2a363e3c81
DanielB <d.banasiewicz@pdo.com.pl>
2021-10-27 20:52:25 +0200

    third example is done

--- ThirdExample/scripts/App.js
+++ ThirdExample/scripts/App.js
@@ -1,12 +1,38 @@
-class App extends React.Component {
+class Counter extends React.Component {
 
-    state = {}
+    state = {
+        result: 1,
+        ratio: 2,
+    }
+
+    handleMultiplication = () => {
+        this.setState((state) => ({
+            result: state.result * state.ratio,
+        }))
+    }
+
+    componentDidUpdate() {
+        if (this.state.result > 1000 && this.state.ratio === 2) {
+            this.setState({
+                ratio: .5,
+            })
+        } else if (this.state.result < 1 && this.state.ratio === .5) {
+            this.setState({
+                ratio: 2,
+            })
+        }
+    }
 
     render() {
         return (
-            <div></div>
+            <div>
+                <p>Kalkulator mnoży przez dwa, jeśli suma jest mniejsza niż 1000. Po przekroczeniu 1000 kalkulator mnoży
+                    przez 0.5 aż osiągnie sumę mniejszą niż jeden. Wtedy ponownie zaczyna mnożyć przez dwa.</p>
+                <button onClick={this.handleMultiplication}>{`Pomnóż przez ${this.state.ratio}`}</button>
+                <h1>Wynik: {this.state.result}</h1>
+            </div>
         )
     }
 }
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<Counter/>, document.getElementById('root'));
